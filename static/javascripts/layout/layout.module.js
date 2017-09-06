061c0a21d2fbbcc8ebc2fcf9653a2dc43e460b66
nsbradford <nsbradford@gmail.com>
2017-09-05 22:06:24 -0400

    Add particles to home page, but text is not overlayed properly.

--- static/javascripts/layout/layout.module.js
+++ static/javascripts/layout/layout.module.js
@@ -3,9 +3,13 @@
 
   angular
     .module('projecthub.layout', [
-      'projecthub.layout.controllers'
+      'projecthub.layout.controllers',
+      'projecthub.layout.directives'
     ]);
 
   angular
     .module('projecthub.layout.controllers', []);
+  
+  angular
+    .module('projecthub.layout.directives', []);
 })();
