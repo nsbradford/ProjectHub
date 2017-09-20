f090597a62948be27ce5120ff43ff936e2c4ecb4
Ben Bianchi <bgbianchi@wpi.edu>
2017-09-20 19:58:01 -0400

    Added eslint to the npm packages. Created an .eslintrc so we can have a coding standard. Changed some task names within Gulp File

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,35 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+/**
+ * Project Hub Gulp File
+ * 
+ * 1. build   -- ngAnnotate and uglify the JS.
+ * 2. serve   -- Compile Scss and use Browse-Sync to show changes
+ * 3. sass    -- Compile Scss
+ * 4. default -- build and serve
+ */
+
+let gulp        = require('gulp');
+let ngAnnotate = require('gulp-ng-annotate');
+let uglify = require('gulp-uglify');
+let browserSync = require('browser-sync').create();
+let sass        = require('gulp-sass');
 
 
-gulp.task('serve', ['sass'], function() {
+gulp.task('build', function () {
+  return gulp.src('static/javascripts/**/*.js')
+    .pipe(ngAnnotate())
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/static/javascripts/'));
+});
+
+gulp.task('serve', ['sass'], function () {
 
   browserSync.init({
     notify: false,
-    proxy: "localhost:8000/"
+    proxy: 'localhost:8000/'
 });
 
 
-    gulp.watch('static/stylesheets/sass/**/*.scss', ['sass',browserSync.reload]);
+    gulp.watch('static/stylesheets/sass/**/*.scss', ['sass', browserSync.reload]);
     gulp.watch('{authentication,projecthub,projects,static/javascripts,static/templates,templates}/**/*.{scss,js,py,html}', browserSync.reload);
 });
 
@@ -23,4 +41,4 @@ gulp.task('sass', function(){
 });
 
 
-gulp.task('Auto-Compile', ['serve']);
+gulp.task('default', ['serve','build']);
