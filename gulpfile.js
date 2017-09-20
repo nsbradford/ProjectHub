749826acc81dde2c5977fd72eb2f63070f219689
Ben Bianchi <bgbianchi@wpi.edu>
2017-09-20 19:31:55 -0400

    Changed GulpFile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,26 @@
-var gulp = require('gulp');
-var ngAnnotate = require('gulp-ng-annotate');
-var uglify = require('gulp-uglify');
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
 
-gulp.task('default', ['build'], function () {
+
+gulp.task('serve', ['sass'], function() {
+
+  browserSync.init({
+    notify: false,
+    proxy: "localhost:8000/"
 });
 
-gulp.task('build', function () {
-  return gulp.src('static/javascripts/**/*.js')
-    .pipe(ngAnnotate())
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/static/javascripts/'));
+
+    gulp.watch('static/stylesheets/sass/**/*.scss', ['sass',browserSync.reload]);
+    gulp.watch('{authentication,projecthub,projects,static/javascripts,static/templates,templates}/**/*.{scss,js,py,html}', browserSync.reload);
 });
+
+gulp.task('sass', function(){
+  return gulp.src('static/stylesheets/sass/**/*.scss')
+    .pipe(sass())
+    .on('error', sass.logError) 
+    .pipe(gulp.dest('static/stylesheets/css'))
+});
+
+
+gulp.task('default', ['serve']);
