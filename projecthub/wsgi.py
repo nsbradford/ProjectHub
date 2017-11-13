fac925c2153e1e6eb8e90427d45a7b1b3031bec9
nsbradford <nsbradford@gmail.com>
2017-11-12 23:09:12 -0500

    Cleanup wsgi.py

--- projecthub/wsgi.py
+++ projecthub/wsgi.py
@@ -5,14 +5,16 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+
+We've switched to using WhiteNoise, here's the old version using Cling:
+	from django.core.wsgi import get_wsgi_application
+	from dj_static import Cling
+	application = Cling(get_wsgi_application())
 """
 
 import os
-# from django.core.wsgi import get_wsgi_application
-# from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projecthub.settings")
-# application = Cling(get_wsgi_application())
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
