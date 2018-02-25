91448eda881c986854954d1a2977d10771c8460e
nsbradford <nsbradford@gmail.com>
2018-02-25 15:23:18 -0500

    Refactoring tests...

--- tests/projects/tests.py
+++ tests/projects/tests.py
@@ -17,26 +17,26 @@ from projects import views
 
 class ProjectTests(APITestCase):
 
-    email = 'johndoe@wpi.edu'
-    username = 'johndoe'
-    first_name = 'John'
-    last_name = 'Doe'
-    password = 'password123'
+    # email = 'johndoe@wpi.edu'
+    # username = 'johndoe'
+    # first_name = 'John'
+    # last_name = 'Doe'
+    # password = 'password123'
     author = 'myauthor'
     title = 'mytitle'
     description = 'mydescription'
     majors = ['Underwater Basket Weaving', 'Computer Science']
     tags = ['Academic', 'Startup']
-    tagline = 'This is the tagline for John Doe #1'
+    # tagline = 'This is the tagline for John Doe #1'
     new_title = title + '-edited!'
     new_description = description + '-edited!'
     setup_data = {
-                'email': email,
-                'username': username,
-                'password': password,
-                'first_name': first_name,
-                'last_name': last_name,
-                'tagline': tagline
+                'email': 'johndoe@wpi.edu',
+                'username': 'johndoe',
+                'password': 'password123',
+                'first_name': 'John',
+                'last_name': 'Doe',
+                'tagline': 'This is the tagline for John Doe #1'
             }
     project_data = {
         'title': title,
@@ -66,28 +66,64 @@ class ProjectTests(APITestCase):
         return ProjectTests.project_url + str(account.pk) + '/'
 
 
+    # def setUp(self):
+    #     """
+    #     In this setup function, we provide django some definitions of Majors
+    #     Some existing accounts, and one existing project to test our edits, and
+    #     deletes on.
+    #     """
+    #     Major.objects.create(title='Underwater Basket Weaving')
+    #     Major.objects.create(title='Partying')
+    #     # Define a correct User
+    #     self.account_correct = self.client.post(
+    #         self.accounts_url,
+    #         data=self.user_correct_payload,
+    #         format='json'
+    #     )
+    #     # Define a wrong User
+    #     self.account_wrong = self.client.post(
+    #         self.accounts_url,
+    #         data=self.user_wrong_payload,
+    #         format='json'
+    #     )
+
+    #     # Define an existing project that we can edit and delete
+    #     self.login_correct()
+    #     self.project_existing = self.client.post(
+    #         self.project_url,
+    #         data=self.existing_project_payload,
+    #         format='json'
+    #     )
+    #     # We need to logout so we allow the tests to specify:
+    #     # Is there someone even logged in, or who is logged it
+    #     self.client.logout()
+
+
+    def setUp(self):
+        self.setup_account()
+
+        major1 = Major.objects.create(title='Underwater Basket Weaving')
+        major2 = Major.objects.create(title='Computer Science')
+        tag1 = Tag.objects.create(title='Academic')
+        tag2 = Tag.objects.create(title='Startup')
+
     def setup_account(self):
         """ Create an account and confirm it is stored properly.
             Returns:
                 The created Account.
         """
-        self.assertEqual(Account.objects.count(), 0)
-        post_response = self.client.post(self.accounts_url, self.setup_data, format='json')
-        self.assertEqual(post_response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Account.objects.count(), 1)
-        new_account = Account.objects.get()
-        self.assertEqual(new_account.email, self.email)
-        self.assertEqual(new_account.username, self.username)
-        return new_account
+        new_project = Account.objects.create_user(
+                            email=self.setup_data['email'],
+                            username=self.setup_data['username'],
+                            password=self.setup_data['password'],
+                            first_name=self.setup_data['first_name'],
+                            last_name=self.setup_data['last_name'],
+                            tagline=self.setup_data['tagline'])
+        return new_project
 
 
     def setup_project(self, new_account, data_modifier='', create_data=True):
         """ """
-        if create_data:
-            major1 = Major.objects.create(title='Underwater Basket Weaving')
-            major2 = Major.objects.create(title='Computer Science')
-            tag1 = Tag.objects.create(title='Academic')
-            tag2 = Tag.objects.create(title='Startup')
         
         major_queryset = Major.objects.all() #[major1, major2]
         tag_queryset = Tag.objects.all() #[tag1, tag2]    
@@ -109,13 +145,11 @@ class ProjectTests(APITestCase):
         self.assertQuerysetEqual(new_project.tags.all(), map(repr, tag_queryset), ordered=False)
 
 
-    def setup_account_and_project(self, activate=True):
+    def setup_account_and_project(self):
         """ """
-        self.assertEqual(Account.objects.count(), 0)
-        new_account = self.setup_account()
-        if activate:
-            self.activate_account(new_account)
-        self.client.login(email=self.email, password=self.password)
+        new_account = Account.objects.get()
+        self.activate_account(new_account)
+        self.client.login(email=self.setup_data['email'], password=self.setup_data['password'])
         self.setup_project(new_account)
         return new_account
 
@@ -128,17 +162,7 @@ class ProjectTests(APITestCase):
 
     def test_create_project_must_be_activated(self):
         """ Get 403 Forbidden if posting from an account without an activated email."""
-        self.setup_account()
-
-        major1 = Major.objects.create(title='Underwater Basket Weaving')
-        major2 = Major.objects.create(title='Computer Science')
-        major_queryset = [major1, major2]
-
-        tag1 = Tag.objects.create(title='Academic')
-        tag2 = Tag.objects.create(title='Startup')
-        tag_queryset = [tag1, tag2]
-
-        self.client.login(email=self.email, password=self.password)
+        self.client.login(email=self.setup_data['email'], password=self.setup_data['password'])
         response = self.client.post(self.project_url, self.project_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
@@ -184,7 +208,7 @@ class ProjectTests(APITestCase):
         get_response = self.client.get(ProjectTests.make_project_url(new_account))
         self.assertEqual(get_response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(get_response.data), 8)
-        self.assertEqual(get_response.data['author']['username'], self.username)
+        self.assertEqual(get_response.data['author']['username'], self.setup_data['username'])
         self.assertEqual(get_response.data['title'], self.title)
         self.assertEqual(get_response.data['description'], self.description)
         self.assertIn('created_at', get_response.data)
@@ -197,7 +221,7 @@ class ProjectTests(APITestCase):
         new_account = self.setup_account_and_project()
         self.setup_project(new_account, '-appendtext', create_data=False)
         self.assertEqual(len(Project.objects.all()), 2)
-        get_response = self.client.get('/api/v1/accounts/' + self.username + '/projects/')
+        get_response = self.client.get('/api/v1/accounts/' + self.setup_data['username'] + '/projects/')
         self.assertEqual(get_response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(get_response.data), 2) # should return 2 projects
         sorted_projects = sorted(get_response.data, key=lambda x: x.get('title'))
@@ -319,37 +343,7 @@ class ProjectTests(APITestCase):
 #     def get_existing_project_url():
 #         return ProjectTests.project_url + str(1) + '/'
 
-#     def setUp(self):
-#         """
-#         In this setup function, we provide django some definitions of Majors
-#         Some existing accounts, and one existing project to test our edits, and
-#         deletes on.
-#         """
-#         Major.objects.create(title='Underwater Basket Weaving')
-#         Major.objects.create(title='Partying')
-#         # Define a correct User
-#         self.account_correct = self.client.post(
-#             self.accounts_url,
-#             data=self.user_correct_payload,
-#             format='json'
-#         )
-#         # Define a wrong User
-#         self.account_wrong = self.client.post(
-#             self.accounts_url,
-#             data=self.user_wrong_payload,
-#             format='json'
-#         )
 
-#         # Define an existing project that we can edit and delete
-#         self.login_correct()
-#         self.project_existing = self.client.post(
-#             self.project_url,
-#             data=self.existing_project_payload,
-#             format='json'
-#         )
-#         # We need to logout so we allow the tests to specify:
-#         # Is there someone even logged in, or who is logged it
-#         self.client.logout()
 
 #     def test_create_project_must_be_activated(self):
 #         """ Get 403 Forbidden if posting from an account without an activated email."""
