d2cca1e13120ea136ed84cf76942a51670d4d52d
nsbradford <nsbradford@gmail.com>
2018-02-25 16:11:05 -0500

    Further refactoring of tests...

--- tests/projects/tests.py
+++ tests/projects/tests.py
@@ -25,7 +25,7 @@ class ProjectTests(APITestCase):
     author = 'myauthor'
     title = 'mytitle'
     description = 'mydescription'
-    majors = ['Underwater Basket Weaving', 'Computer Science']
+    # majors = ['Underwater Basket Weaving', 'Computer Science']
     tags = ['Academic', 'Startup']
     # tagline = 'This is the tagline for John Doe #1'
     new_title = title + '-edited!'
@@ -41,24 +41,18 @@ class ProjectTests(APITestCase):
     project_data = {
         'title': title,
         'description': description,
-        'majors': majors,
+        'majors': ['Underwater Basket Weaving', 'Computer Science'],
         'tags': tags
     }
     project_data_edited = {
         'title': new_title,
         'description': new_description,
-        'majors': majors,
+        'majors': ['Underwater Basket Weaving', 'Computer Science'],
         'tags': tags
     }
     accounts_url = '/api/v1/accounts/'
     project_url = '/api/v1/projects/'
     url_activate = '/api/v1/auth/activate/%s'
-
-
-    def activate_account(self, new_account):
-        good_token = new_account.get_confirmation_key()
-        post_response = self.client.post(self.url_activate % good_token)
-        self.assertEqual(post_response.status_code, status.HTTP_202_ACCEPTED)
 
 
     @staticmethod
@@ -99,41 +93,53 @@ class ProjectTests(APITestCase):
     #     self.client.logout()
 
 
+    def activate_account(self, new_account):
+        good_token = new_account.get_confirmation_key()
+        post_response = self.client.post(self.url_activate % good_token)
+        self.assertEqual(post_response.status_code, status.HTTP_202_ACCEPTED)
+
+
+    def login(self):
+        self.client.login(email=self.setup_data['email'], password=self.setup_data['password'])
+
+
     def setUp(self):
         self.setup_account()
-
         major1 = Major.objects.create(title='Underwater Basket Weaving')
         major2 = Major.objects.create(title='Computer Science')
         tag1 = Tag.objects.create(title='Academic')
         tag2 = Tag.objects.create(title='Startup')
 
+
     def setup_account(self):
-        """ Create an account and confirm it is stored properly.
-            Returns:
-                The created Account.
-        """
-        new_project = Account.objects.create_user(
+        """ Create an account."""
+        Account.objects.create_user(
                             email=self.setup_data['email'],
                             username=self.setup_data['username'],
                             password=self.setup_data['password'],
                             first_name=self.setup_data['first_name'],
                             last_name=self.setup_data['last_name'],
                             tagline=self.setup_data['tagline'])
-        return new_project
 
 
     def setup_project(self, new_account, data_modifier='', create_data=True):
-        """ """
-        
-        major_queryset = Major.objects.all() #[major1, major2]
-        tag_queryset = Tag.objects.all() #[tag1, tag2]    
+        """ Create a new project. """
 
-        self.assertEqual(Major.objects.count(), 2)
+        project = Project.objects.create(
+                                author=new_account,
+                                title=self.project_data['title'],
+                                description=self.project_data['description'],
+                            )
+        project.majors = Major.objects.all()
+        project.tags = Tag.objects.all()
 
+
+    def create_project(self, new_account, data_modifier='', create_data=True):
+        """ Create a new project and confirm the fields are set correct. """
         previous_n_projects = Project.objects.count()
         self.assertEqual(Account.objects.count(), 1)
         modified_project_data = dict(self.project_data)
-        modified_project_data['title'] = modified_project_data['title'] + data_modifier
+        modified_project_data['title'] += data_modifier
         response = self.client.post(self.project_url, modified_project_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Project.objects.count(), previous_n_projects + 1)
@@ -141,15 +147,15 @@ class ProjectTests(APITestCase):
         self.assertEqual(new_project.author, new_account)
         self.assertEqual(new_project.title, self.title + data_modifier)
         self.assertEqual(new_project.description, self.description)
-        self.assertQuerysetEqual(new_project.majors.all(), map(repr, major_queryset), ordered=False)
-        self.assertQuerysetEqual(new_project.tags.all(), map(repr, tag_queryset), ordered=False)
+        self.assertQuerysetEqual(new_project.majors.all(), map(repr, Major.objects.all()), ordered=False)
+        self.assertQuerysetEqual(new_project.tags.all(), map(repr, Tag.objects.all()), ordered=False)
 
 
-    def setup_account_and_project(self):
+    def activate_login_and_setup_project(self):
         """ """
         new_account = Account.objects.get()
         self.activate_account(new_account)
-        self.client.login(email=self.setup_data['email'], password=self.setup_data['password'])
+        self.login()
         self.setup_project(new_account)
         return new_account
 
@@ -169,12 +175,15 @@ class ProjectTests(APITestCase):
 
     def test_create_project(self):
         """ Ensure we can create a new project object. """
-        self.setup_account_and_project()
+        new_account = Account.objects.get()
+        self.activate_account(new_account)
+        self.login()
+        self.create_project(new_account)
 
 
     def test_delete_project_must_be_authenticated(self):
         """ Can't delete a project when not signed in. """
-        new_account = self.setup_account_and_project()
+        new_account = self.activate_login_and_setup_project()
         self.client.logout()
         response = self.client.delete(ProjectTests.make_project_url(new_account))
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
@@ -182,7 +191,8 @@ class ProjectTests(APITestCase):
 
     def test_delete_project(self):
         """ DELETE a single project. """
-        new_account = self.setup_account_and_project()
+        self.activate_login_and_setup_project()
+        new_account = Account.objects.get()
         self.assertEqual(len(Project.objects.all()), 1)
         response = self.client.delete(ProjectTests.make_project_url(new_account))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
@@ -191,7 +201,8 @@ class ProjectTests(APITestCase):
 
     def test_update_project(self):
         """ PUT to edit a project. """
-        new_account = self.setup_account_and_project()
+        self.activate_login_and_setup_project()
+        new_account = Account.objects.get()
         self.assertEqual(len(Project.objects.all()), 1)
         get_response = self.client.put(ProjectTests.make_project_url(new_account),
                             self.project_data_edited, format='json')
@@ -203,23 +214,28 @@ class ProjectTests(APITestCase):
 
     def test_get_single_project(self):
         """ GET a single project by author and project name. """
-        new_account = self.setup_account_and_project()
+        self.activate_login_and_setup_project()
+        new_account = Account.objects.get()
         self.assertEqual(len(Project.objects.all()), 1)
         get_response = self.client.get(ProjectTests.make_project_url(new_account))
         self.assertEqual(get_response.status_code, status.HTTP_200_OK)
+
         self.assertEqual(len(get_response.data), 8)
         self.assertEqual(get_response.data['author']['username'], self.setup_data['username'])
         self.assertEqual(get_response.data['title'], self.title)
-        self.assertEqual(get_response.data['description'], self.description)
-        self.assertIn('created_at', get_response.data)
-        self.assertIn('updated_at', get_response.data)
-        self.assertEqual(sorted(get_response.data['majors']), sorted(map(unicode, self.majors)))
-        self.assertEqual(sorted(get_response.data['tags']), sorted(map(unicode, self.tags)))
+        self.assertEqual(
+            sorted(get_response.data['majors']), 
+            sorted(map(unicode, self.project_data['majors']))
+        )
+        self.assertEqual(
+            sorted(get_response.data['tags']), 
+            sorted(map(unicode, self.tags))
+        )
 
 
     def test_get_all_projects_by_user(self):
-        new_account = self.setup_account_and_project()
-        self.setup_project(new_account, '-appendtext', create_data=False)
+        new_account = self.activate_login_and_setup_project()
+        self.create_project(new_account, '-appendtext', create_data=False)
         self.assertEqual(len(Project.objects.all()), 2)
         get_response = self.client.get('/api/v1/accounts/' + self.setup_data['username'] + '/projects/')
         self.assertEqual(get_response.status_code, status.HTTP_200_OK)
