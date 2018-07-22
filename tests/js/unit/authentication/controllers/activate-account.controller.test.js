b2ba39bcd2c42f9f5110fbc67c0f5206040f5e88
Ben Bianchi <bgb319@gmail.com>
2018-07-22 17:07:44 -0400

    More docs in activate-account controller test

--- tests/js/unit/authentication/controllers/activate-account.controller.test.js
+++ tests/js/unit/authentication/controllers/activate-account.controller.test.js
@@ -16,29 +16,39 @@ describe('ActivateAccountController', function () {
     beforeEach(module('projecthub.authentication.controllers'));
     let ActivateAccountController, LocationMock, ScopeMock, RouteParamsMock, SnackBarMock, AuthenticationMock, DialogMock;
 
+    /**
+     * Construct all mocked services, and spy on them so that we know if they were called or not within
+     * a test
+     */
     beforeEach(loadDependencies = function () {
+
+        // Location is called on the promise reject for activating an account
         LocationMock = {
-            url: function(string) {}
+            url: function (string) { }
         };
 
         spyOn(LocationMock, "url").and.callThrough();
 
+        // Initialize a mock that we can spy on later
         AuthenticationMock = {
             activateAccount: function (key) { },
         };
 
+        // Mock to see if a dialog is called or not.
         DialogMock = {
             open: function (dialogData) { return dialogData; },
         };
 
         spyOn(DialogMock, "open");
 
+        // RouteParams are required to instantiate this controller
         RouteParamsMock = {
             key: "key"
         };
 
+        // Snack Bar is called on a promise reject. Required for controller instantiation
         SnackBarMock = {
-            error: function(string) {}
+            error: function (string) { }
         };
 
         spyOn(SnackBarMock, "error").and.callThrough();
@@ -49,7 +59,11 @@ describe('ActivateAccountController', function () {
     describe('Success Case', function () {
         beforeEach(setAuthToSucceed = function () {
             spyOn(AuthenticationMock, "activateAccount").and.returnValue(
-                Promise.resolve(true)
+                {
+                    then: function (success, failure) {
+                        success({});
+                    }
+                }
             );
         });
 
@@ -66,11 +80,15 @@ describe('ActivateAccountController', function () {
             });
         }));
 
-        it('Should Instantiate', function() {
+        /**
+         * The success of this test is more difficult, than you think. It helps to have a
+         * test that confirms that the test file is actually valid.
+         */
+        it('Should Instantiate', function () {
             expect(ActivateAccountController).toBeDefined();
         });
 
-        it('Should open a dialog on success and forward the user to the login page', function (){
+        it('Should open a dialog on success and forward the user to the login page', function () {
             ScopeMock.$digest();
             AuthenticationMock.activateAccount().then(function () {
                 expect(DialogMock.open).toHaveBeenCalled();
@@ -80,11 +98,17 @@ describe('ActivateAccountController', function () {
 
     });
 
+    /**
+     * Seperate Failure Case so that we can reuse as much code as possible
+     */
     describe('Failure Case', function () {
         beforeEach(setAuthToFail = function () {
             spyOn(AuthenticationMock, "activateAccount").and.returnValue(
-                Promise.reject(true)
-            );
+                {
+                    then: function (success, failure) {
+                        failure({});
+                    }
+                });
         });
 
         beforeEach(angular.mock.inject(function ($rootScope, $controller) {
@@ -100,17 +124,13 @@ describe('ActivateAccountController', function () {
             });
         }));
 
-        it('Should Instantiate', function() {
+        it('Should Instantiate', function () {
             expect(ActivateAccountController).toBeDefined();
         });
 
-        it('Should open a SnackBar error on failure and forward the user to the root page', function (){
-            ScopeMock.$digest();
-            AuthenticationMock.activateAccount().then(function (result) {
-                expect(SnackBarMock.error).toHaveBeenCalled();
-                expect(LocationMock.url).toHaveBeenCalled();
-            });
-
+        it('Should open a SnackBar error on failure and forward the user to the root page', function () {
+            expect(SnackBarMock.error).toHaveBeenCalled();
+            expect(LocationMock.url).toHaveBeenCalled();
         });
 
     });
